5aa35e618748507844321599d27b279751a65839
Red and Green <62441426+RGGH@users.noreply.github.com>
2022-06-18 12:49:42 +0100

    Update sample1.py

--- ipAddress/sample1.py
+++ ipAddress/sample1.py
@@ -1,4 +1,14 @@
-from ipaddress import IPv4Address
+''' Tabulate Network Information '''
+
+from ipaddress import IPv4Network, IPv4Interface, IPv4Address
+from tabulate import tabulate
+
+vlans = {"sw1": [10, 20, 30, 40], "sw2": [1, 2, 10], "sw3": [1, 2, 3, 4, 5, 10, 11, 12]}
+print(tabulate(vlans, headers="keys"))
+
+
+
+''' Parse IPv4 Network Information '''
 
 addr = IPv4Address("220.14.9.37")
 
@@ -6,14 +16,12 @@ print(addr)
 print(type(addr))
 
 
-from ipaddress import IPv4Network
 net = IPv4Network("192.4.2.0/24")
 print(net.num_addresses)
 
 net = net.netmask
 print(net)
 
-from ipaddress import IPv4Interface
 ifc = IPv4Interface("192.168.1.6/24")
 print(f"ip = {ifc.ip}")
 print(f"network = {ifc.network}")
